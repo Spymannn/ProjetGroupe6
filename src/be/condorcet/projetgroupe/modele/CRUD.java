988d87e7ce679f4f22d13566d2cccd4c1bcdccde
Spymannn <samir_hanini@live.be>
2014-11-12 11:37:17 +0100

    Version 1

---
+++ src/be/condorcet/projetgroupe/modele/CRUD.java
@@ -0,0 +1,9 @@
+package be.condorcet.projetgroupe.modele;
+
+public interface CRUD {
+	  public void create()throws Exception;
+	  public void read() throws Exception;
+	  public void update() throws Exception;
+	  public void delete() throws Exception;
+	}
+
