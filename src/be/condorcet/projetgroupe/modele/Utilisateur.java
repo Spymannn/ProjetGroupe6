e0458c9fb48a557dd565fdef931220c86a4228bc
roberto.randazzo <roberto.randazzo@hotmail.be>
2014-11-18 10:49:59 +0100

    fin création utilisateur et début utilisateurDB

--- src/be/condorcet/projetgroupe/modele/Utilisateur.java
+++ src/be/condorcet/projetgroupe/modele/Utilisateur.java
@@ -1,5 +1,260 @@
 package be.condorcet.projetgroupe.modele;
 
-public class Utilisateur {
+import java.sql.Date;
 
+public class Utilisateur {
+	/**
+	 * identifiant unique de l'utilisateur
+	 */
+	protected int idUser;
+	/**
+	 * nom de l'utilisateur
+	 */
+	protected String nomUser;
+	/**
+	 * prenom de l'utilisateur
+	 */
+	protected String prenomUser;
+	/**
+	 * mot de passe 
+	 */
+	protected String mdp;
+	/**
+	 * pseudo de l'utilisateur
+	 */
+	protected String pseudoUser;
+	/**
+	 * email de l'utilisateur
+	 */
+	protected String email;
+	/**
+	 * date de naissance de l'utilisateur
+	 */
+	protected Date dateNaissance;
+	/**
+	 * genre de l'utilisateur
+	 */
+	protected String gender;
+	/**
+	 * sport favoris 1
+	 */
+	protected String sportFavoris1;
+	/**
+	 * sport favoris 2
+	 */
+	protected String sportFavoris2;
+	/**
+	 * sport favoris 3
+	 */
+	protected String sportFavoris3;
+	/**
+	 * constructeur par défaut
+	 */
+	public Utilisateur(){
+		
+	}
+	/**
+	 * constructeur paramétré
+	 * @param idUser
+	 * @param nomUser
+	 * @param prenomUser
+	 * @param mdp
+	 * @param pseudoUser
+	 * @param email
+	 * @param dateNaissance
+	 * @param gender
+	 * @param sportFavoris1
+	 * @param sportFavoris2
+	 * @param sportFavoris3
+	 */
+	public Utilisateur(int idUser, String nomUser, String prenomUser,
+			String mdp, String pseudoUser, String email, Date dateNaissance,
+			String gender, String sportFavoris1, String sportFavoris2,
+			String sportFavoris3) {
+		super();
+		this.idUser = idUser;
+		this.nomUser = nomUser;
+		this.prenomUser = prenomUser;
+		this.mdp = mdp;
+		this.pseudoUser = pseudoUser;
+		this.email = email;
+		this.dateNaissance = dateNaissance;
+		this.gender = gender;
+		this.sportFavoris1 = sportFavoris1;
+		this.sportFavoris2 = sportFavoris2;
+		this.sportFavoris3 = sportFavoris3;
+	}
+
+	/**
+	 * getter idUser
+	 * @return
+	 */
+	public int getIdUser() {
+		return idUser;
+	}
+	/**
+	 * setter idUser
+	 * @param idUser
+	 */
+	public void setIdUser(int idUser) {
+		this.idUser = idUser;
+	}
+	/**
+	 * getter nomUser
+	 * @return
+	 */
+	public String getNomUser() {
+		return nomUser;
+	}
+	/**
+	 * setter nomUser
+	 * @param nomUser
+	 */
+	public void setNomUser(String nomUser) {
+		this.nomUser = nomUser;
+	}
+	/**
+	 * getter prenomUser
+	 * @return
+	 */
+	public String getPrenomUser() {
+		return prenomUser;
+	}
+	/**
+	 * setter prenomUser
+	 * @param prenomUser
+	 */
+	public void setPrenomUser(String prenomUser) {
+		this.prenomUser = prenomUser;
+	}
+	/**
+	 * getter mdp
+	 * @return
+	 */
+	public String getMdp() {
+		return mdp;
+	}
+	/**
+	 * setter mdp
+	 * @param mdp
+	 */
+	public void setMdp(String mdp) {
+		this.mdp = mdp;
+	}
+	/**
+	 * getter pseudoUser
+	 * @return
+	 */
+	public String getPseudoUser() {
+		return pseudoUser;
+	}
+	/**
+	 * setter pseudoUser
+	 * @param pseudoUser
+	 */
+	public void setPseudoUser(String pseudoUser) {
+		this.pseudoUser = pseudoUser;
+	}
+	/**
+	 * getter email
+	 * @return
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * setter email
+	 * @param email
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	/**
+	 * getter dateNaissance
+	 * @return
+	 */
+	public Date getDateNaissance() {
+		return dateNaissance;
+	}
+	/**
+	 * setter dateNaissance
+	 * @param dateNaissance
+	 */
+	public void setDateNaissance(Date dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+	/**
+	 * getter gender
+	 * @return
+	 */
+	public String getGender() {
+		return gender;
+	}
+	/**
+	 * setter gender
+	 * @param gender
+	 */
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	/**
+	 * getter sportFavoris1
+	 * @return
+	 */
+	public String getSportFavoris1() {
+		return sportFavoris1;
+	}
+	/**
+	 * setter sportFavoris1
+	 * @param sportFavoris1
+	 */
+	public void setSportFavoris1(String sportFavoris1) {
+		this.sportFavoris1 = sportFavoris1;
+	}
+	/**
+	 * getter sportFavoris2
+	 * @return
+	 */
+	public String getSportFavoris2() {
+		return sportFavoris2;
+	}
+	/**
+	 * setter sportFavoris2
+	 * @param sportFavoris2
+	 */
+	public void setSportFavoris2(String sportFavoris2) {
+		this.sportFavoris2 = sportFavoris2;
+	}
+	/**
+	 * getter sportFavoris3
+	 * @return
+	 */
+	public String getSportFavoris3() {
+		return sportFavoris3;
+	}
+	/**
+	 * setter sportFavoris3
+	 * @param sportFavoris3
+	 */
+	public void setSportFavoris3(String sportFavoris3) {
+		this.sportFavoris3 = sportFavoris3;
+	}
+	/**
+	 * méthode toString()
+	 */
+	@Override
+	public String toString() {
+		return "Utilisateur [idUser=" + idUser + ", nomUser=" + nomUser
+				+ ", prenomUser=" + prenomUser + ", mdp=" + mdp
+				+ ", pseudoUser=" + pseudoUser + ", email=" + email
+				+ ", dateNaissance=" + dateNaissance + ", gender=" + gender
+				+ ", sportFavoris1=" + sportFavoris1 + ", sportFavoris2="
+				+ sportFavoris2 + ", sportFavoris3=" + sportFavoris3 + "]";
+	}
+	
+	
+	
+	
+	
+	
 }
--- src/be/condorcet/projetgroupe/modele/UtilisateurDB.java
+++ src/be/condorcet/projetgroupe/modele/UtilisateurDB.java
@@ -1,5 +1,195 @@
 package be.condorcet.projetgroupe.modele;
 
-public class UtilisateurDB {
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import android.util.Log;
+
+public class UtilisateurDB extends Utilisateur implements CRUD{
+	/**
+	  * connexion à la base de données partagée entre toutes les instances(statique)
+	  */
+	protected static Connection dbConnect = null;
+	/**
+	 * Constructeur par défaut
+	 */
+	public UtilisateurDB(){
+		
+	}
+	/**
+	 * Constructeur paramétré
+	 * @param idSport
+	 * @param nomSport
+	 */
+	public UtilisateurDB(int idUser, String nomUser, String prenomUser,
+			String mdp, String pseudoUser, String email, Date dateNaissance,
+			String gender, String sportFavoris1, String sportFavoris2,
+			String sportFavoris3){
+		super(idUser,nomUser, prenomUser, mdp, pseudoUser, email, dateNaissance, gender, sportFavoris1, sportFavoris2, sportFavoris3);
+		
+	}
+	/**
+	   * méthode statique permettant de partager la connexion entre toutes les instances de
+	   * UtilisateurDB
+	   * @param nouvdbConnect connexion à la base de données
+	   */
+	public static void setConnection(Connection nouvdbConnect){
+		dbConnect = nouvdbConnect;
+	}
+	/**
+	 * Création d'un nouvel utilisateur
+	 */
+	@Override
+	public void create() throws Exception {
+		CallableStatement cstmt=null;
+		
+	       try{
+		     String req = "call createSport(?,?,?,?,?,?,?,?,?,?,?)";
+		     cstmt = dbConnect.prepareCall(req);
+	         cstmt.setString(nomUser, prenomUser, mdp, pseudoUser, email, (java.sql.Date)dateNaissance, gender, sportFavoris1, sportFavoris2, sportFavoris3);
+		     cstmt.executeUpdate();
+	       }
+	       catch(Exception e){
+	    	   throw new Exception("Erreur de création "+e.getMessage());
+	       }
+	       finally{//effectué dans tous les cas
+
+	    	   try{
+	    		   cstmt.close();
+	            }
+	    	   catch(Exception e){}
+	        }
+	}
+	/**
+	 * Lecture d'un utilisateur par rapport à son nom 
+	 * permet de récupérer son id
+	 */
+	@Override
+	public void read() throws Exception {
+		String req = "select * from utilisateur where nomUser  = ?"; 
+       PreparedStatement  pstmt=null;
+       try{
+       	pstmt=dbConnect.prepareStatement(req);
+       	pstmt.setString(1,nomUser);
+    	    ResultSet rs=(ResultSet)pstmt.executeQuery();	
+       	
+       	    
+        if(rs.next()){
+	     	this.idUser=rs.getInt("idUser");
+	     	//this.nomUser = nomUser;
+	     	}
+	      else { 
+	             throw new Exception("Code inconnu");
+	      }
+
+           }
+	catch(Exception e){
+		 Log.d("connexion","erreur"+e);   
+               throw new Exception("Erreur de lecture "+e.getMessage());
+            }
+       finally{//effectué dans tous les cas 
+           try{
+             pstmt.close();
+           }
+           catch (Exception e){}
+       }
+		
+	}
+	/**
+	 * Lecture de tous les utilisateurs
+	 * @return
+	 * @throws Exception
+	 */
+	public static ArrayList<UtilisateurDB> afficheTousUtilisateurs()throws Exception{
+	    ArrayList<UtilisateurDB> utilisateur=new ArrayList<UtilisateurDB>();
+	    String req = "select * from utilisateur"; 
+       PreparedStatement  pstmt=null;
+	    try{
+	    	pstmt=dbConnect.prepareStatement(req);
+    	    ResultSet rs=(ResultSet)pstmt.executeQuery();
+    	    boolean ok = false;
+           while(rs.next()){
+           	ok = true;
+           	int idUser = rs.getInt("idUser");
+           	String nomUser = rs.getString("nomUser");
+           	String prenomUser = rs.getString("prenomUser");
+           	
+           	utilisateur.add(new UtilisateurDB(idUser,nomUser, prenomUser));
+	      }
+	   
+             if(!ok)throw new Exception("nom inconnu");
+             else return utilisateur;
+	     }
+	     catch(Exception e){
+		
+               throw new Exception("Erreur de lecture "+e.getMessage());
+            }
+           finally{//effectué dans tous les cas 
+           try{
+             pstmt.close();
+           }
+           catch (Exception e){}
+       }
+    }
+	/**
+	 * Update du nom d'un utilisateur
+	 */
+	@Override
+	public void update() throws Exception {
+		CallableStatement cstmt=null;
+
+	    try{
+		     String req = "call updatesportnom(?,?)";
+		     cstmt=dbConnect.prepareCall(req);
+		     PreparedStatement pstm = dbConnect.prepareStatement(req);
+		     cstmt.setInt(1,idUser);
+		     cstmt.setString(2,nomUser);
+		     cstmt.executeUpdate();
+	            
+	    }
+
+		  catch(Exception e){
+		  	
+	                throw new Exception("Erreur de mise à jour "+e.getMessage());
+	             }
+	          finally{//effectué dans tous les cas 
+	            try{
+	              cstmt.close();
+	            }
+	            catch (Exception e){}
+	        }
+		
+	}
+	
+	/**
+	 * Delete d'un utilisateur via son id
+	 */
+	@Override
+	public void delete() throws Exception {
+		 CallableStatement cstmt =null;
+		   try{
+		     String req = "call deleteUser(?)";
+		     cstmt = dbConnect.prepareCall(req);
+		     cstmt.setInt(1,idUser);
+		     cstmt.executeUpdate();
+		     	     
+		     }	
+		   catch (Exception e){
+		     	
+	                throw new Exception("Erreur d'effacement "+e.getMessage());
+	             }
+	           finally{//effectué dans tous les cas 
+	            try{
+	              cstmt.close();
+	            }
+	            catch (Exception e){}
+	          }
+		
+	}
 
 }
+
