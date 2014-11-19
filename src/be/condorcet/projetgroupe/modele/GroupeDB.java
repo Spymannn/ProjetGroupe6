ce4a3d3e48628c03dcbaaa4f3b7220ec67c1fcb1
corentin.speckens <cspeckens@gmail.com>
2014-11-19 09:55:31 +0100

    TestGroupeDB

--- src/be/condorcet/projetgroupe/modele/GroupeDB.java
+++ src/be/condorcet/projetgroupe/modele/GroupeDB.java
@@ -15,25 +15,195 @@ public class GroupeDB extends Groupe implements CRUD {
 		super(idGroupe,nomGroupe,mdpGroupe,idSport,admin,maxUser,nbrUser);
 	}
 	
+	public GroupeDB(String nomGroupe, String mdpGroupe, int idSport, int admin, int maxUser){
+		super(0,nomGroupe,mdpGroupe,idSport,admin,maxUser,0);
+	}
+	
+	public GroupeDB(int idGroupe) {
+		super(idGroupe,"","",0,0,0,0);
+	}
+	
+	public GroupeDB(String nomGroupe) {
+		super(0,nomGroupe,"",0,0,0,0);
+	}
+	
+	public static void setConnection(Connection nouvdbConnect){
+		dbConnect = nouvdbConnect;
+	}
+	
 	@Override
-	public void delete() throws Exception {
+	public void create() throws Exception {
+		CallableStatement cstmt=null;
 		try {
-			
+			String req = "call createGroupe(?,?,?,?,?)";
+			cstmt = dbConnect.prepareCall(req);
+			cstmt.setString(1, nomGroupe);
+			cstmt.setString(2, mdpGroupe);
+			cstmt.setInt(3, admin);
+			cstmt.setInt(4, maxUser);
+			cstmt.setInt(5, idSport);
+			cstmt.executeUpdate();
 		}
 		catch (Exception e){
-			throw new Exception("Erreur");
+			throw new Exception("Erreur de création"+e.getMessage());
+		}
+		finally {
+			try {
+				cstmt.close();
+			}
+			catch(Exception e) {}
 		}
 	}
 	
-	public void create() throws Exception {
-		
+	public void delete() throws Exception {
+		CallableStatement cstmt = null;
+		try {
+			String req = "call deleteGroupe(?)";
+			cstmt = dbConnect.prepareCall(req);
+			cstmt.setInt(1, idGroupe);
+			cstmt.executeUpdate();
+		}
+		catch (Exception e){
+			throw new Exception("Erreur de suppression"+e.getMessage());
+		}
+		finally {
+			try {
+				cstmt.close();
+			}
+			catch (Exception e) {}
+		}
 	}
 	
 	public void read() throws Exception {
-		
+		String req = "select * from groupe where nomGroupe = ?";
+		PreparedStatement pstmt=null;
+		try {
+			pstmt=dbConnect.prepareStatement(req);
+			pstmt.setString(1, nomGroupe);
+			ResultSet rs = (ResultSet)pstmt.executeQuery();
+			
+			if(rs.next()) {
+				this.idGroupe=rs.getInt("idGroupe");
+				this.mdpGroupe=rs.getString("mdpGroupe");
+				this.idSport=rs.getInt("idSport");
+				this.admin=rs.getInt("admin");
+				this.maxUser=rs.getInt("maxUser");
+				this.nbrUser=rs.getInt("nbrUser");
+			}
+			else {
+				throw new Exception ("Code Inconnu");
+			}
+		}
+		catch(Exception e) {
+			throw new Exception("Erreur de lecture "+e.getMessage());
+		}
+		finally {
+			try {
+				pstmt.close();
+			}
+			catch (Exception e) {}
+		}
 	}
 	
+	public static ArrayList<GroupeDB> afficheTousGroupe() throws Exception {
+		ArrayList<GroupeDB> groupes=new ArrayList<GroupeDB>();
+		String req = "select * from groupe";
+		PreparedStatement pstmt=null;
+		try {
+			pstmt=dbConnect.prepareStatement(req);
+			ResultSet rs=(ResultSet)pstmt.executeQuery();
+			boolean ok = false;
+			while(rs.next()) {
+				ok = true;
+				int idGroupe = rs.getInt("idGroupe");
+				String nomGroupe = rs.getString("nomGroupe");
+				String mdpGroupe = rs.getString("mdpGroupe");
+				int idSport = rs.getInt("idSport");
+				int admin = rs.getInt("admin");
+				int maxUser = rs.getInt("maxUser");
+				int nbrUser = rs.getInt("nbrUser");
+				groupes.add(new GroupeDB(idGroupe,nomGroupe,mdpGroupe,idSport,admin,maxUser,nbrUser));
+			}
+			if(!ok)throw new Exception("nom inconnu");
+			else return groupes;
+		}
+		catch (Exception e) {
+			throw new Exception ("Erreur de lecture "+e.getMessage());
+		}
+		finally {
+			try {
+				pstmt.close();
+			}
+			catch (Exception e) {}
+		}
+	}
+	
+	public void updateNom() throws Exception {
+		CallableStatement cstmt = null;
+		try {
+			String req = "call updateGroupeNom(?,?)";
+			cstmt=dbConnect.prepareCall(req);
+			PreparedStatement pstm = dbConnect.prepareStatement(req);
+			cstmt.setInt(1, idGroupe);
+			cstmt.setString(2, nomGroupe);
+			cstmt.executeUpdate();
+		}
+		catch (Exception e) {
+			throw new Exception ("Erreur d update N "+e.getMessage());
+		}
+		finally {
+			try {
+				cstmt.close();
+			}
+			catch (Exception e) {}
+		}
+	}
+	
+	public void updateMaxUser() throws Exception {
+		CallableStatement cstmt = null;
+		try {
+			String req = "call updateGroupeMaxUser(?,?)";
+			cstmt=dbConnect.prepareCall(req);
+			PreparedStatement pstm = dbConnect.prepareStatement(req);
+			cstmt.setInt(1, idGroupe);
+			cstmt.setInt(2, maxUser);
+			cstmt.executeUpdate();
+		}
+		catch (Exception e) {
+			throw new Exception ("Erreur d'update M "+e.getMessage());
+		}
+		finally {
+			try {
+				cstmt.close();
+			}
+			catch (Exception e) {}
+		}
+	}
+	
+	public void updatePassword() throws Exception {
+		CallableStatement cstmt = null;
+		try {
+			String req = "call updateGroupePassword (?,?)";
+			cstmt=dbConnect.prepareCall(req);
+			PreparedStatement pstm = dbConnect.prepareStatement(req);
+			cstmt.setInt(1, idGroupe);
+			cstmt.setString(2, mdpGroupe);
+			cstmt.executeUpdate();
+		}
+		catch (Exception e) {
+			throw new Exception ("Erreur d'update P "+e.getMessage());
+		}
+		finally {
+			try {
+				cstmt.close();
+			}
+			catch (Exception e) {}
+		}
+	}
+
+	@Override
 	public void update() throws Exception {
+		// TODO Auto-generated method stub
 		
 	}
 }
---
+++ src/be/condorcet/projetgroupe/modele/TestGroupeDB.java
@@ -0,0 +1,35 @@
+package be.condorcet.projetgroupe.modele;
+
+import java.sql.*;
+import java.util.*;
+
+public class TestGroupeDB {
+
+	public static void main(String[] args) {
+		DBConnection dbc = new DBConnection();
+		Connection con = dbc.getConnection();
+		if(con==null) {
+			System.out.println("Connexion impossible");
+			System.exit(0);
+		}
+		else {
+			System.out.println("Connexion reussi");
+			GroupeDB.setConnection(con);
+			GroupeDB gp1;
+			
+			try {
+				System.out.println("\nTest d'ajout d'un groupe");
+				System.out.println("===================================");
+				gp1 = new GroupeDB("Footi","sport",61,20,2);
+				gp1.create();
+				
+				System.out.println("GP1 : "+gp1);
+				System.out.println("Groupe creee");
+			}
+			catch(Exception e){
+				System.err.println("Erreur de creation d un groupe");
+			}
+		}
+	}
+
+}
