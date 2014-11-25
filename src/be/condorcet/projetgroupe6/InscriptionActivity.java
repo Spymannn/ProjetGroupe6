a910a49d3fbc949bacce0313d736bd150367cdea
Spymannn <samir_hanini@live.be>
2014-11-25 08:50:43 +0100

    Debut Use Case 1

--- src/be/condorcet/projetgroupe6/InscriptionActivity.java
+++ src/be/condorcet/projetgroupe6/InscriptionActivity.java
@@ -1,46 +1,22 @@
 package be.condorcet.projetgroupe6;
 
-
-
 import android.support.v7.app.ActionBarActivity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.TextView;
 
-public class MainActivity extends ActionBarActivity {
-	TextView signup = null;
+public class InscriptionActivity extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		signup = (TextView)findViewById(R.id.signup);
-		signup.setOnClickListener(
-					new OnClickListener(){
-						public void onClick(View v){
-						
-						//Intent i1 = new Intent(Activite1.this,Activite4.class);
-						Intent i2 = new Intent(MainActivity.this,InscriptionActivity.class);
-						//i1.putExtra("donneur", don);
-						
-						startActivity(i2);
-						//test changement Corentin
-						
-					}
-				}
-				
-				
-				);
+		setContentView(R.layout.activity_inscription);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.inscription, menu);
 		return true;
 	}
 
